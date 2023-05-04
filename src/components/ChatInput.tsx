d3ee493daecc485b425cfcf3a4858088afe37969
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-05-04 04:13:52 -0500

    configured context messges

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -23,8 +23,19 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
       });
       return response.body;
     },
-    onSuccess: () => {
-      console.log("succes");
+    onSuccess: async (stream) => {
+      if (!stream) throw new Error("No stream found");
+
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        console.log("chukValue ", chunkValue);
+      }
     },
   });
   return (
