4f03685508395e91d86640c6ddfac46da16e0388
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-05-04 03:17:43 -0500

    chatgpt intrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,28 @@
-import Chat from '@/components/Chat'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Chat from "@/components/Chat";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Providers from "@/components/Provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Bookbudy',
-  description: 'Your book  for fantasy & mystery novels',
-}
+  title: "Bookbudy",
+  description: "Your book  for fantasy & mystery novels",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Chat />
-        {children}</body>
+      <Providers>
+        <body className={inter.className}>
+          <Chat />
+          {children}
+        </body>
+      </Providers>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className='absolute inset-0 flex justify-center items-center'>Website content</main>
-  )
+    <main className="absolute inset-0 flex justify-center items-center">
+      Website content
+    </main>
+  );
 }
